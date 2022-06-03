cd0112da61060131223a29643e7f3839dea0fa98
maureenndiema <ndiemam@gmail.com>
2022-06-03 20:53:45 +0300

    creating class Profile

--- gram/models.py
+++ gram/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    username = models.CharField(max_length =30)
+    bio = models.TextField()
+    dp = models.ImageField( blank=True)
+    user = models.OneToOneField(User,related_name='profile', on_delete=models.CASCADE, primary_key=True)
