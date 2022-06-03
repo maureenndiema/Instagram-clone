5d2909bc394eb9becff1c486444b3d4c5b06c7dc
maureenndiema <ndiemam@gmail.com>
2022-06-03 21:04:34 +0300

    function for save profile

--- gram/models.py
+++ gram/models.py
@@ -23,3 +23,6 @@ class Profile(models.Model):
     @receiver(post_save, sender=User)
     def save_user_profile(sender,instance, **kwargs):
         instance.profile.save()
+
+    def save_profile(self):
+        self.save()  
