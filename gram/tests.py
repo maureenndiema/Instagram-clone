2a3e9abe0050afc557d1625301ffbb3e7fa86f04
maureenndiema <ndiemam@gmail.com>
2022-06-04 11:44:05 +0300

    profile test class

--- gram/tests.py
+++ gram/tests.py
@@ -2,3 +2,12 @@ from django.test import TestCase
 from .models import Image,Profile
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+
+    """
+    Test profile class and its functions
+    """
+    def setUp(self):
+
+        self.user = User.objects.create(id =1,username='hannah')
+        self.profile = Profile(dp='', bio='', contact=" ",user=self.user)
