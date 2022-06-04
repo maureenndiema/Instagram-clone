e69249117bd50033364c81d7279b7ba1bdb02ad9
maureenndiema <ndiemam@gmail.com>
2022-06-04 15:36:42 +0300

    importing datetime and classes

--- gram/views.py
+++ gram/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 from django.contrib.auth import login, authenticate
+from .models import Image,Profile,Comments
+import datetime as dt
 
 # Create your views here.
