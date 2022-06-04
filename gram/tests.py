8e89a18f6a406066f35be2989b15a0bb0a6f6569
maureenndiema <ndiemam@gmail.com>
2022-06-04 12:26:05 +0300

    test to delete image

--- gram/tests.py
+++ gram/tests.py
@@ -86,5 +86,11 @@ class ImageTestClass(TestCase):
         images  = Image.objects.all()
         self.assertTrue(len(images)>0)
 
+    def test_delete_method(self):
+        self.image.save_image()
+        self.image.delete_image()
+        image = Image.objects.all()
+        self.assertTrue(len(image) > 0)
+
         
 
