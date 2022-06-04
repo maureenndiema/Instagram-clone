fd39473f6f6aabdfe4d8b84020f66f0b9c63d54b
maureenndiema <ndiemam@gmail.com>
2022-06-04 16:08:06 +0300

    function that searches for profiles usernames

--- gram/views.py
+++ gram/views.py
@@ -84,3 +84,18 @@ def new_image(request):
         form = NewImageForm()
     return render(request, 'new_image.html', {"form": form})
 
+  
+@login_required(login_url='/accounts/login/')
+def search_user(request):
+   """
+   Function that searches for profiles based on the usernames
+   """
+   if 'username' in request.GET and request.GET["username"]:
+       name = request.GET.get("username")
+       searched_profiles = User.objects.filter(username__icontains=name)
+       message = f"{name}"
+       profiles = User.objects.all()
+       people = Follow.objects.following(request.user)
+       print(profiles)
+       return render(request, 'all-insta/search.html', {"message":message, "usernames":searched_profiles, "profiles":profiles,})  
+
